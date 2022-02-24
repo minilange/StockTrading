1dd36559622e381d8c7b28611022292dfe4cc3bf
manlw <manlw@vestas.com>
2022-02-24 11:57:10 +0100

    stock trading options

--- StockTrading/src/app/components/transaction-log/testdata.ts
+++ StockTrading/src/app/components/transaction-log/testdata.ts
@@ -48,5 +48,52 @@ export const testData: ITransaction[] = [
         operation: "buy",
         stock: "test",
         transactor: "Carl Lee Ladefoged"
+    },{
+        timeStamp: "2022-02-24T08:34:25Z",
+        newPrice: 10.0,
+        oldPrice: 9.0,
+        operation: "buy",
+        stock: "test",
+        transactor: "Carl Lee Ladefoged"
+    },
+    {
+        timeStamp: "2022-02-24T08:35:25Z",
+        newPrice: 11.0,
+        oldPrice: 10.0,
+        operation: "buy",
+        stock: "test",
+        transactor: "Carl Lee Ladefoged"
+    },
+    {
+        timeStamp: "2022-02-24T08:34:25Z",
+        newPrice: 10.0,
+        oldPrice: 9.0,
+        operation: "buy",
+        stock: "test",
+        transactor: "Carl Lee Ladefoged"
+    },
+    {
+        timeStamp: "2022-02-24T08:34:25Z",
+        newPrice: 10.0,
+        oldPrice: 12.0,
+        operation: "sell",
+        stock: "test",
+        transactor: "Carl Lee Ladefoged"
+    },
+    {
+        timeStamp: "2022-02-24T08:34:25Z",
+        newPrice: 13.0,
+        oldPrice: 9.0,
+        operation: "buy",
+        stock: "test",
+        transactor: "Carl Lee Ladefoged"
+    },
+    {
+        timeStamp: "2022-02-24T08:34:25Z",
+        newPrice: 100.0,
+        oldPrice: 9.0,
+        operation: "buy",
+        stock: "test",
+        transactor: "Carl Lee Ladefoged"
     }   
 ]
